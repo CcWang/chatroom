0141a645cc34753aea6f363e9f52666907c54be8
Chongchong Wang <chongchong1110@gmail.com>
2016-05-26 00:03:54 -0700

    alert new user, show all user list

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -21,4 +21,39 @@ $(document).ready(function(){
 	if (socket) {
 		console.log('jQuery works');
 	}
+	$('#enter').click(function(){
+		var name = prompt('Welcome, please enter your name:');
+		if (name == '') {
+			name = prompt('Welcome, please enter your name:');
+		}else{
+			socket.emit('new_user',{name:name});
+			$('#enter').hide();
+		}
+	})
+	socket.on('new_user',function(data){
+		// console.log(data);
+		var welcome = '';
+		for(var key in data.new_user){
+			// console.log(data.yourself[key]);
+			welcome+=data.new_user[key]+' has joined the room.';
+			var list = '<li>'+data.new_user[key]+'</li>';
+		}
+		alert(welcome);
+		$('#list').append(list);
+	})
+	socket.on('users',function(data){
+		console.log('users',data);
+		var list='<ul id="list">';
+		for(var i=0;i<data.all_user.length;i++){
+			for(var key in data.all_user[i]){
+				console.log(data.all_user[i][key])
+				list += '<li>'+data.all_user[i][key]+'</li>';
+			}
+		}
+		list+='</ul>'
+		$('#user_list').html(list);
+	})
+	socket.on('feed_back',function(data){
+		$('p').html(data.answer);
+	})
 })
--- server.js
+++ server.js
@@ -14,9 +14,23 @@ var server = app.listen(8899,function () {
 	// body...
 	console.log('listening on 8899 for chatroom');
 })
-
+var users =[];
 var io = require('socket.io').listen(server);
 io.sockets.on('connection',function(socket){
 	console.log('socket'+socket.id + 'is connected');
+	socket.on('new_user',function(data){
+		console.log(data.name);
+		var user ={}
+		user[socket.id] = data.name;
+		users.push(user);
+		socket.emit('users',{all_user:users});
+		socket.broadcast.emit('new_user',{new_user:user})
+	});
+	//disconnect
+	socket.on('disconnect',function(){
+		// var i = all_user.indexOf(socket.id);
+		// all_user.splice(i,1);
+		io.emit(console.log('by'));
+	})
 
 })
